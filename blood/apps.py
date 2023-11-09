f114873a1a7c722155e7a818813dee10acbf406c
Mohd. Daniyal Hasan <mohddaniyalh@gmail.com>
2023-11-09 19:42:01 +0530

    initial commit

---
+++ blood/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BloodConfig(AppConfig):
+    name = 'blood'
+    default_auto_field = 'django.db.models.BigAutoField'
---
+++ donor/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render,redirect,reverse
+from . import forms,models
+from django.db.models import Sum,Q
+from django.contrib.auth.models import Group
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required,user_passes_test
+from django.conf import settings
+from datetime import date, timedelta
+from django.core.mail import send_mail
+from django.contrib.auth.models import User
+from blood import forms as bforms
+from blood import models as bmodels
+
+def donor_signup_view(request):
+    userForm=forms.DonorUserForm()
+    donorForm=forms.DonorForm()
+    mydict={'userForm':userForm,'donorForm':donorForm}
+    if request.method=='POST':
+        userForm=forms.DonorUserForm(request.POST)
+        donorForm=forms.DonorForm(request.POST,request.FILES)
+        if userForm.is_valid() and donorForm.is_valid():
+            user=userForm.save()
+            user.set_password(user.password)
+            user.save()
+            donor=donorForm.save(commit=False)
+            donor.user=user
+            donor.bloodgroup=donorForm.cleaned_data['bloodgroup']
+            donor.save()
+            my_donor_group = Group.objects.get_or_create(name='DONOR')
+            my_donor_group[0].user_set.add(user)
+        return HttpResponseRedirect('donorlogin')
+    return render(request,'donor/donorsignup.html',context=mydict)
+
+def donor_dashboard_view(request):
+    donor= models.Donor.objects.get(user_id=request.user.id)
+    dict={
+        'requestpending': models.BloodDonate.objects.all().filter(donor=donor).filter(status='Pending').count(),
+        'requestapproved': models.BloodDonate.objects.all().filter(donor=donor).filter(status='Approved').count(),
+        'requestmade': models.BloodDonate.objects.all().filter(donor=donor).count(),
+        'requestrejected': models.BloodDonate.objects.all().filter(donor=donor).filter(status='Rejected').count(),
+        'username': models.Donor.objects.get(user_id=request.user.id).get_name
+    }
+    return render(request,'donor/donor_dashboard.html',context=dict)
+
+def donate_blood_view(request):
+    donation_form=forms.DonationForm()
+    if request.method=='POST':
+        donation_form=forms.DonationForm(request.POST)
+        if donation_form.is_valid():
+            blood_donate=donation_form.save(commit=False)
+            donor= models.Donor.objects.get(user_id=request.user.id)
+            blood_donate.donor=donor
+            blood_donate.bloodgroup=donor.bloodgroup
+            blood_donate.save()
+            return HttpResponseRedirect('donation-history')  
+    return render(request,'donor/donate_blood.html',{'donation_form':donation_form})
+
+def donation_history_view(request):
+    donor= models.Donor.objects.get(user_id=request.user.id)
+    donations=models.BloodDonate.objects.all().filter(donor=donor)
+    return render(request,'donor/donation_history.html',{'donations':donations})
